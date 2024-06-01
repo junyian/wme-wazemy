018a431a87bcb5fd314d136c750728a2ef6a779b
munyeenkong <17055182@siswa.um.edu.my>
2024-06-01 22:57:48 +0800

    Add Waze W typings.

--- src/typings.d.ts
+++ src/typings.d.ts
@@ -1,1 +1,40 @@
 declare module "*.less";
+
+interface WmeState {
+  isInitialMapDataLoaded: boolean; // if `wme-map-data-loaded` has already been dispatched
+  isInitialized: boolean; // if `wme-initialzied` has already been dispatched
+  isReady: boolean; // if `wme-ready` has already been dispatched
+}
+
+interface RegisterSidebarTabResult {
+  tabLabel: HTMLElement;
+  tabPane: HTMLElement;
+}
+
+interface W {
+  userscripts: {
+    state: WmeState;
+    registerSidebarTab(scriptId: string): RegisterSidebarTabResult;
+    waitForElementConnected(el: HTMLElement): Promise<void>;
+    removeSidebarTab(scriptId: string): void;
+    getDataModelByMapElement(
+      el: HTMLElement | SVGElement,
+    ): DataModelObject | null;
+    getMapElementByDataModel(
+      model: DataModelObject,
+    ): HTMLElement | SVGElement | null;
+    getDataModelByMarkerElement(markerEl: HTMLElement): DataModelObject | null;
+    getMarkerElementByDataModel(model: DataModelObject): HTMLElement | null;
+    getDataModelByFeatureElement(featureEl: SVGElement): DataModelObject | null;
+    getFeatureElementByDataModel(model: DataModelObject): SVGElement | null;
+    toGeoJSONGeometry(
+      geometry: OpenLayers.Geometry | GeoJSON.Geometry,
+    ): GeoJSON.Geometry;
+    toOLGeometry(
+      geometry: OpenLayers.Geometry | GeoJSON.Geometry,
+    ): OpenLayers.Geometry;
+    convertWktToGeoJSON(geometry: GeoJSON.Geometry): string; // WKT string
+    convertGeoJSONToWkt(geometry: string /* WKT string */): GeoJSON.Geometry;
+  };
+}
+declare var W: W;
