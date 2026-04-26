f8e525750682ae74cebcf94dcd8ab68416112f4e
Lean Jun Yian <junyian@Leans-MacBook-Pro.local>
2026-04-26 06:37:52 +0200

    chore: add flake.nix, bun.lock and update .gitignore

---
+++ flake.nix
@@ -0,0 +1,33 @@
+{
+  description = "A very basic flake for Bun.js projects";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs =
+    {
+      self,
+      nixpkgs,
+      flake-utils,
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            bun
+            nodejs
+          ];
+
+          shellHook = ''
+            echo "Bun.js development environment"
+          '';
+        };
+      }
+    );
+}
